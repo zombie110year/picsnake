0455fb079ece03b2d915ea871810d9dc464f1014
zombie110year <zombie110year@outlook.com>
2020-02-20 20:24:27 +0800

    功能（site）：向 sm.ms 上传图片

---
+++ picsnake/site/__init__.py
@@ -0,0 +1,7 @@
+from .sm_ms import ISession as SmmsSession
+
+__all__ = ("SmmsSession", )
+
+INTERGRATED_BEDS = {
+    "sm.ms",
+}
---
+++ picsnake/site/sm_ms.py
@@ -0,0 +1,97 @@
+"""sm.ms 图床 API
+"""
+import asyncio
+import json
+from datetime import datetime
+from mimetypes import guess_type
+from pathlib import PurePath
+from typing import Tuple
+import aiofiles
+from aiohttp import ClientSession
+from aiohttp import CookieJar
+from aiohttp import FormData
+
+__all__ = ("ISession", )
+
+
+class ISession:
+    """https://doc.sm.ms/
+    """
+    URL_PREFIX = "https://sm.ms/api/v2/"
+    AUTH_KEYNAME = "Authorization"
+
+    def __init__(self, username=None, password=None):
+        self._headers = dict()
+        self._cookies = CookieJar()
+        self._token = None
+
+        if None not in (username, password):
+            # 登录账户
+            asyncio.run(self._login(username, password))
+
+    async def _login(self, username, password):
+        """获取 token，添加到 Header 中
+
+        Authorization: <token>
+        """
+        async with ClientSession(headers=self._headers,
+                                 cookies=self._cookies) as cs:
+            async with cs.post(self.api("/token"),
+                               json={
+                                   "username": username,
+                                   "password": password
+                               }) as response:
+                if response.status == 200:
+                    json: dict = await response.json()
+                    self.token = json["data"]["token"]
+                    self.headers[self.AUTH_KEYNAME] = self.token
+                    print("[{now}] login: sm.ms - {username}".format(
+                        now=datetime.now(), username=username))
+            async with cs.post(self.api("/profile"),
+                               json={self.AUTH_KEYNAME:
+                                     self.token}) as response:
+                if response.status == 200:
+                    json: dict = await response.json()
+                    print("Login    : {}".format(json["data"]["username"]))
+                    dur: int = json["data"]["disk_usage_raw"]
+                    du: str = json["data"]["disk_usage"]
+                    dlr: int = json["data"]["disk_limit_raw"]
+                    dl: str = json["data"]["disk_limit"]
+                    print("Disk rest: {:.3f}%, {} / {}".format(
+                        dur / dlr, du, dl))
+
+    async def upload(self, filepath: str) -> Tuple[str, str]:
+        """返回（访问链接，删除链接）
+
+        sm.ms 图床的上传功能需要向 ``/upload`` POST 一个含字段 ``smfile`` 的 FormData.
+        FormData 中可以分别传入参数::
+
+            field="smfile", value=<文件内容>, filename=<文件名>, content_type=<mime type>
+
+        来上传文件。
+
+        .. note::
+
+            sm.ms 返回的响应是满足 JSON 格式的字节，不能直接用 Response.json 解码。
+        """
+
+        async with ClientSession(headers=self._headers,
+                                 cookies=self._cookies) as cs:
+            async with aiofiles.open(filepath, "rb") as source:
+                form = FormData()
+                filename = PurePath(filepath).name
+                mimetype = guess_type(filepath)[0]  # 返回的是 (type, encoding)
+                form.add_field(
+                    "smfile",
+                    value=(await source.read()),
+                    content_type=mimetype,
+                    filename=filename,
+                )
+                async with cs.post(self.api("/upload"), data=form) as response:
+                    if response.status == 200:
+                        rawdata = await response.read()
+                        return_data = json.loads(rawdata)
+        return return_data["data"]["url"], return_data["data"]["delete"]
+
+    def api(self, name: str) -> str:
+        return "{}{}".format(self.URL_PREFIX, name)
