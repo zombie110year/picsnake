62d8c4a64e3ecd11ec6b702d931f7c2f59b613a3
zombie110year <zombie110year@outlook.com>
2020-02-20 15:27:21 +0800

    功能（model）：数据模型

---
+++ picsnake/model.py
@@ -0,0 +1,39 @@
+from hashlib import sha256
+
+import databases
+import ormantic as orm
+import sqlalchemy
+
+__all__ = "UploadedPicture", "StoredPicture"
+
+SHA256_LENGTH = 64
+# TODO 读取配置
+DATABASE = databases.Database("sqlite:///db.sqlite")
+METADATA = sqlalchemy.MetaData()
+
+Sha256Hex: orm.String = orm.String(primary_key=True,
+                                   index=True,
+                                   unique=True,
+                                   max_length=SHA256_LENGTH)
+BedName: orm.String = orm.String(max_length=127)
+Url: orm.String = orm.String(max_length=0xffffffff)
+UpTime: orm.DateTime = orm.DateTime()
+
+
+class UploadedPicture(orm.Model):
+    """被上传的图片，记录字段
+
+    - hash（HEX）：str
+    - bed（图床的别名、用来对应解析规则）：str
+    - url（链接地址）：str
+    - uptime（上传时间）：datetime
+    """
+    hash: Sha256Hex
+    bed: BedName
+    url: Url
+    uptime: UpTime
+
+    class Mapping:
+        table_name = "upload"
+        metadata = DATABASE
+        database = METADATA
