8f003067a6586672f32ebc07c847d30b9ce4a8bd
zombie110year <zombie110year@outlook.com>
2020-02-20 22:38:25 +0800

    修改 model，适应图片管理需求
    1. 新增 filename: 以便日后导出
    2. access, delete 改名: 命名冲突

--- picsnake/model.py
+++ picsnake/model.py
@@ -15,9 +15,8 @@ METADATA = sqlalchemy.MetaData()
 
 Sha256Hex: orm.String = orm.String(primary_key=True,
                                    index=True,
-                                   unique=True,
                                    max_length=SHA256_LENGTH)
-
+BedName: orm.String = orm.String(primary_key=True, max_length=127)
 ShortName: orm.String = orm.String(max_length=127)
 Url: orm.String = orm.String(max_length=0xffffffff)
 DateTime: orm.DateTime = orm.DateTime()
@@ -27,16 +26,19 @@ class UploadedPicture(orm.Model):
     """被上传的图片，记录字段
 
     - hash（HEX）：str
+    - filename（文件名）：str
     - bed（图床的别名、用来对应解析规则）：str
-    - access（如何访问图片）：str
-    - delete（如何删除图片）：str
+    - accesser（如何访问图片）：str
+    - deleter（如何删除图片）：str
     - uptime（上传时间）：datetime
+
+    以一个对象对应一个图床上的一张图片。
     """
     hash: Sha256Hex
+    bed: BedName
     filename: ShortName
-    bed: ShortName
-    access: Url
-    delete: Url
+    accesser: Url
+    deleter: Url
     uptime: DateTime
 
     class Mapping:
