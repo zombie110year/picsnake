86a7e7a9fe2f1cc41c057205f866f6d561d11d3a
zombie110year <zombie110year@outlook.com>
2020-02-20 21:37:43 +0800

    功能（util）：ReadableImageFile 会缓存自己的内容，只读取一次

--- picsnake/util.py
+++ picsnake/util.py
@@ -1,15 +1,24 @@
-from .abc import ReadableImageFileABC
+import asyncio
 from pathlib import PurePath
+from typing import Union
+
 from aiofiles import open as open_async
+
+from .abc import ReadableImageFileABC
 
 __all__ = ("ReadabelImageFile", )
 
 
 class ReadableImageFile(ReadableImageFileABC):
     def __init__(self, filepath: str):
-        self.path = filepath
-        self.filename = PurePath(filepath).name
+        self.path: str = filepath
+        self.filename: str = PurePath(filepath).name
+        self.content: Union[bytes, None] = None
+        self.lock = asyncio.Lock()
 
     async def read(self):
-        with open_async(self.path, "rb") as instream:
-            return await instream.read()
+        if self.content is None:
+            with self.lock:
+                with open_async(self.path, "rb") as instream:
+                    self.content = await instream.read()
+        return self.content
