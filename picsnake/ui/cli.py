44cdeb60a1c26b8ef547a0c26bee7551ad1d134d
zombie110year <zombie110year@outlook.com>
2020-02-23 22:52:44 +0800

    添加回显功能

--- picsnake/ui/cli.py
+++ picsnake/ui/cli.py
@@ -1,11 +1,14 @@
 import asyncio
-from argparse import ArgumentParser
-from typing import *
 import json
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import *
+
 from .. import __app_name__
 from ..site import INTERGRATED_BEDS
 from ..upload import ImageUploader
-from ..util import const_expr, split_large_list
+from ..util import const_expr
+from ..util import split_large_list
 
 __all__ = ("cli_main", "cli_args")
 
@@ -19,17 +22,13 @@ def parser():
     upload = subcmd.add_parser("upload", aliases=["u", "up"])
     upload.add_argument("filepath", help="指定要上传的文件（可多个）", nargs="+")
 
-    upload.add_argument("--dry-run",
-                        help="如果指定此标志，则不会真实操作，只会演示效果",
-                        action="store_true")
-    upload.add_argument("--bed",
-                        help="指定图床",
-                        choices=INTERGRATED_BEDS.keys(),
-                        default="sm.ms")
-    upload.add_argument(
-        "--bedargs",
-        help=("针对图床的参数，输入 JSON"),
-        metavar="{'username': 'example', 'password': 'example'}")
+    upload.add_argument("--dry-run", help="如果指定此标志，则不会真实操作，只会演示效果", action="store_true")
+    upload.add_argument("--bed", help="指定图床", choices=INTERGRATED_BEDS.keys(), default="sm.ms")
+    upload.add_argument("--bedargs",
+                        help=("针对图床的参数，输入 JSON"),
+                        metavar="{'username': 'example', 'password': 'example'}")
+    upload.add_argument("--no-echo", help="上传文件后不回显链接", action="store_true")
+    upload.add_argument("--echo-style", help="回显链接样式", default="md", choices=["md", "raw"])
 
     # 定义子命令执行器
     upload.set_defaults(fn=upload_async)
@@ -42,11 +41,8 @@ def parser():
 
     # todo
     delete = subcmd.add_parser("delete", aliases=["d"])
-    delete.add_argument("expr",
-                        help="表示要删除对象的条件表达式, 语法与 search 所用的相同，文档见 <todo>")
-    delete.add_argument("--dry-run",
-                        help="如果指定此标志，则不会真实操作，只会演示效果",
-                        action="store_true")
+    delete.add_argument("expr", help="表示要删除对象的条件表达式, 语法与 search 所用的相同，文档见 <todo>")
+    delete.add_argument("--dry-run", help="如果指定此标志，则不会真实操作，只会演示效果", action="store_true")
     # 定义子命令执行器
     delete.set_defaults(fn=delete_async)
     return p
@@ -73,20 +69,22 @@ async def upload_async(args):
     - dry_run: bool = False
     - bed: str = "sm.ms"
     - bedargs: List[str] = []
+    - no_echo: bool = False
+    - echo_style: str = args.echo_style
     """
     filepath: List[str] = args.filepath
     dry_run: bool = args.dry_run
     bed: str = args.bed
-    bedargs: dict = json.loads(args.bedargs)
+    bedargs: dict = json.loads(args.bedargs) if args.bedargs is not None else None
+    no_echo: bool = args.no_echo
+    echo_style: str = args.echo_style
 
     if bedargs is None:
         iu = ImageUploader(bed)
     else:
-        iu = ImageUploader(bed,
-                           username=bedargs.get("username", None),
-                           password=bedargs.get("password", None))
+        iu = ImageUploader(bed, username=bedargs.get("username", None), password=bedargs.get("password", None))
 
-    aws = [iu.upload(fp) for fp in filepath]
+    aws = [upload_task(iu, fp, not no_echo, echo_style) for fp in filepath]
     if len(aws) < 10:
         task = asyncio.gather(*aws)
         await task
@@ -104,3 +102,17 @@ async def search_async(args):
 
 async def delete_async(args):
     raise NotImplementedError
+
+
+async def upload_task(iu: ImageUploader, fp: str, echo: bool = False, style: str = "md"):
+    """一个图片的上传任务
+
+    :param iu: 一个配置好的 ImageUploader 实例
+    :param fp: 一个图片文件的路径
+    :param echo: 是否在上传成功后回显链接地址
+    :param style: 回显链接的样式，可选 md, raw
+    """
+    access = await iu.upload(fp)
+    if echo:
+        msg = {"md": lambda: f"![{Path(fp).stem}]({access})", "raw": lambda: access}.get(style, lambda: access)()
+        print(msg)
