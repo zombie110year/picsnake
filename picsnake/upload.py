6505559565b236667b1f71fdd24cbe43a7aed8cd
zombie110year <zombie110year@outlook.com>
2020-02-23 22:14:50 +0800

    upload 时创建图片和上传关系

--- picsnake/upload.py
+++ picsnake/upload.py
@@ -2,11 +2,11 @@
 """
 import asyncio
 from datetime import datetime
-from hashlib import sha256
 from pathlib import Path
 
 import ormantic as orm
 
+from .model import Picture
 from .model import UploadedPicture
 from .settings import Settings
 from .site import INTERGRATED_BEDS
@@ -27,41 +27,38 @@ class ImageUploader:
     """
     bed: ImageBedSessionABC
 
-    def __init__(self,
-                 bed: str = "sm.ms",
-                 username: str = None,
-                 password: str = None):
+    def __init__(self, bed: str = "sm.ms", username: str = None, password: str = None):
         self.bedname = bed
         if bed in INTERGRATED_BEDS.keys():
-            self.bed = INTERGRATED_BEDS[bed]() if all([
-                x is None for x in (username, password)
-            ]) else INTERGRATED_BEDS[bed](username=username, password=password)
+            self.bed = INTERGRATED_BEDS[bed]() if all([x is None
+                                                       for x in (username, password)]) else INTERGRATED_BEDS[bed](
+                                                           username=username, password=password)
         else:
-            raise NotImplementedError("暂不支持自定义图床，目前支持的图床有 {}".format(
-                [name for name in INTERGRATED_BEDS.keys()]))
+            raise NotImplementedError("暂不支持自定义图床，目前支持的图床有 {}".format([name for name in INTERGRATED_BEDS.keys()]))
 
     async def upload(self, imgpath: str):
+        """如果图片是第一次被管理，那么将其添加到 picture 表中
+        """
         fileobj = ReadableImageFile(imgpath)
         task_upload = asyncio.create_task(self.bed.upload(fileobj))
         content = await fileobj.read()
-        hex256: str = sha256(content).hexdigest()
+        hex256: str = (await fileobj.sha256()).hexdigest()
         access, delete = await task_upload
         key = "sha256:{}".format(hex256)
+
         try:
-            exists: UploadedPicture = await UploadedPicture.objects.get(
-                hash=key)
-            print("重复的 {hash!r} @ {filename!r}, 最初上传时间 {uptime}.".format(
-                **exists.dict()))
+            pic: Picture = await Picture.objects.get(hash=key)
+        except orm.NoMatch:
+            await Picture.objects.create(hash=key, filename=fileobj.filename, comment="")
+
+        try:
+            exists: UploadedPicture = await UploadedPicture.objects.get(hash=key)
         except orm.NoMatch:
             success: UploadedPicture = await UploadedPicture.objects.create(
                 hash=key,
                 bed=self.bedname,
-                filename=fileobj.filename,
                 accesser=access,
                 deleter=delete,
                 uptime=datetime.now(),
             )
-            Path(Path(Settings.BLOB_DIR) / hex256).write_bytes(await
-                                                               fileobj.read())
-            print("成功创建条目 ({bed!r}) {hash!r} @ {filename!r} {uptime}.".format(
-                **success.dict()))
+            Path(Path(Settings.BLOB_DIR) / hex256).write_bytes(await fileobj.read())
